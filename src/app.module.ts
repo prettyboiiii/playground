6ae1af59c7ebbab090c96c19653896c072f60f31
prettyboi <73166015+prettyboiiii@users.noreply.github.com>
2025-04-28 01:48:37 +0700

    feat: integrate cache management for optimized revenue calculation in OrdersService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule, OrdersModule],
+  imports: [
+    // TODO: For Scalability's sake, use distributed cache system e.g. Redis instead.
+    CacheModule.register({ isGlobal: true }),
+    ProductsModule,
+    PrismaModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,6 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { OrderItem } from 'src/orders/dto/calculate-total-revenue.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,24 +9,62 @@ export class OrdersService {
   constructor(
     @Inject(PrismaService)
     private readonly prismaService: Pick<PrismaService, 'product'>,
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
   ) {}
 
-  // This will be used in the assignment 2
+  // Optimized version
   async calculateTotalRevenue(orderItems: OrderItem[]) {
-    let total = 0;
+    const productIds = orderItems.map((item) => item.productId);
 
-    for (let i = 0; i < orderItems.length; i++) {
-      const item = orderItems[i];
-      const product = await this.prismaService.product.findUnique({
-        where: { id: item.productId },
+    // Get products from cache, and identify products that need to be fetched from DB
+    const missingIds: string[] = [];
+    await Promise.all(
+      productIds.map((id) =>
+        this.cacheManager.get(`products:${id}`).then((cachedProduct) => {
+          if (!cachedProduct) {
+            missingIds.push(id);
+          }
+        }),
+      ),
+    );
+
+    if (missingIds.length > 0) {
+      const validProducts = await this.prismaService.product.findMany({
+        where: { id: { in: missingIds } },
+        select: { id: true },
       });
-      if (!product) {
-        throw Error('Product Not Found');
-      }
 
-      total += item.price * item.quantity;
+      // Cache the valid products
+      const validProductIdSet = new Set<string>();
+      validProducts.forEach((product) => {
+        validProductIdSet.add(product.id);
+
+        // Set the missing products back to the cacheManager in the fire-and-forget manner to avoid blocking operations
+        // TODO: Set TTL accordingly
+        this.cacheManager.set(`products:${product.id}`, {}).catch((error) => {
+          console.error(
+            `Failed to cache product with ID ${product.id}:`,
+            error,
+          );
+        });
+      });
+
+      // Validate if all product IDs were found
+      const notFoundProducts = missingIds.filter(
+        (id) => !validProductIdSet.has(id),
+      );
+      if (notFoundProducts.length > 0) {
+        throw new Error(
+          `Product Ids Not Found: ${notFoundProducts.join(', ')}`,
+        );
+      }
     }
 
-    return total;
+    // Calculate total revenue based on valid products
+    return orderItems.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0,
+    );
   }
 }
