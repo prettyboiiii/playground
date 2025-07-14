ce1a065da9d3d02ef064e3b4387bdce7ab8f7f8f
prettyboi <73166015+prettyboiiii@users.noreply.github.com>
2025-07-14 23:47:43 +0700

    Init working boilerplate

---
+++ wails/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"embed"
+	"log"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+//go:embed all:frontend/build
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "wails-svelte-demo",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 0, G: 204, B: 204, A: 1},
+		OnStartup:        app.startup,
+		Bind: []any{
+			app,
+		},
+	})
+
+	if err != nil {
+		log.Panic("Error:", err.Error())
+	}
+}
